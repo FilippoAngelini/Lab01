266ac33e17fdb828dc4fcdba6188065bf5ce2c89
jimmy-sonny <s209051@studenti.polito.it>
2017-03-13 13:52:15 +0100

    Update

---
+++ Lab1_Parole_Soluzione/src/it/polito/tdp/parole/model/Parole.java
@@ -0,0 +1,31 @@
+package it.polito.tdp.parole.model;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class Parole {
+	
+	List<String> lista;
+	
+	public Parole() {
+		lista = new LinkedList<String>();
+//		lista = new ArrayList<String>();
+	}
+	
+	public void addParola(String p) {
+		lista.add(p);
+	}
+	
+	public List<String> getElenco() {
+		return lista;
+	}
+	
+	public void reset() {
+		lista.clear();
+	}
+
+	public void removeParola(String string) {
+		lista.remove(string);
+	}
+
+}
